25f94f4d492793f7d4ecad07e47af52703b14d8d
edogola4 <edogola4@gmail.com>
2025-09-15 09:33:41 +0300

    feat: add shared module with common components
    - Add shared components directory
    - Add directives and pipes
    - Set up shared module for reusability

---
+++ src/app/shared/components/online-indicator/online-indicator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-online-indicator',
+  imports: [],
+  templateUrl: './online-indicator.component.html',
+  styleUrl: './online-indicator.component.scss'
+})
+export class OnlineIndicatorComponent {
+
+}
